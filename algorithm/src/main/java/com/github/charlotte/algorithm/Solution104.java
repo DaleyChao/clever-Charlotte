fc956b430f14886ddbc264971802619a06794174
Daniel <daleychao@163.com>
2020-06-03 18:07:14 +0800

    [+]algorithm

---
+++ algorithm/src/main/java/com/github/charlotte/algorithm/Solution104.java
@@ -0,0 +1,47 @@
+package com.github.charlotte.algorithm;
+//给定一个二叉树，找出其最大深度。
+//
+// 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。 
+//
+// 说明: 叶子节点是指没有子节点的节点。 
+//
+// 示例： 
+//给定二叉树 [3,9,20,null,null,15,7]， 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7 
+//
+// 返回它的最大深度 3 。 
+// Related Topics 树 深度优先搜索
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+
+class Solution104 {
+
+    private int depth = 0;
+
+    public int maxDepth(TreeNode root) {
+        max(0, root);
+        return depth;
+    }
+
+
+    public void max(int innerDepth, TreeNode root) {
+        if (root == null) {
+            if (innerDepth > depth) {
+                depth = innerDepth;
+            }
+            return;
+        }
+        max(innerDepth + 1, root.left);
+        max(innerDepth + 1, root.right);
+    }
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
