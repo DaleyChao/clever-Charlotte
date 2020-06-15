3877dc8c9754b4f3231f9bf8b10407fac5f19e11
Daniel <daleychao@163.com>
2020-06-15 11:12:07 +0800

    [+]oa-base

---
+++ oa-system/oa-base/src/main/java/com/github/charlotte/oa/OaBaseApplication.java
@@ -0,0 +1,13 @@
+package com.github.charlotte.oa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OaBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OaBaseApplication.class, args);
+	}
+
+}
---
+++ oa-system/oa-base/src/test/java/com/github/charlotte/oa/OaBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.charlotte.oa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
