4076882a5e206176910e2515a1c0a0e472f9d3fe
Daniel <daleychao@163.com>
2020-06-20 11:50:38 +0800

    [+]dubbo

---
+++ stack/dubbo/demo/dubbo-provider-demo/src/main/java/com/github/charlotte/dubbo/DubboProviderDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.charlotte.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboProviderDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubboProviderDemoApplication.class, args);
+	}
+
+}
