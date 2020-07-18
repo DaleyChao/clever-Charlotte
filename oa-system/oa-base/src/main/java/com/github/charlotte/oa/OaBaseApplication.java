cb89b8080be2285a48458333d08f10739ac01249
Daniel <daleychao@163.com>
2020-07-18 15:02:55 +0800

    [+]starter

--- oa-system/oa-base/src/main/java/com/github/charlotte/oa/OaBaseApplication.java
+++ oa-system/oa-base/src/main/java/com/github/charlotte/oa/OaBaseApplication.java
@@ -1,5 +1,6 @@
 package com.github.charlotte.oa;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Daniel
  */
 @SpringBootApplication
+@MapperScan(value = "com.github.charlotte.oa.dao")
 public class OaBaseApplication {
 
 	public static void main(String[] args) {
