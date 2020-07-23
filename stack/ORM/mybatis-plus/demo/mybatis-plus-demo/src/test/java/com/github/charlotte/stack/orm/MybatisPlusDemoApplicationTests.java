04302ec110582c4c5a0d1f6fc3ffe235929526fe
Daniel <daleychao@163.com>
2020-07-24 00:49:14 +0800

    [+]

---
+++ stack/ORM/mybatis-plus/demo/mybatis-plus-demo/src/test/java/com/github/charlotte/stack/orm/MybatisPlusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.charlotte.stack.orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
