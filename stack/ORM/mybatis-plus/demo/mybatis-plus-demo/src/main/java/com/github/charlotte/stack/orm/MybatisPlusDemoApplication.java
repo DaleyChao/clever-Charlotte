cea9b10608dc5b3ec2ff52b050570c1b898efb84
Daniel <daleychao@163.com>
2020-07-25 00:49:58 +0800

    [+]mybatis plus

---
+++ stack/ORM/mybatis-plus/demo/mybatis-plus-demo/src/main/java/com/github/charlotte/stack/orm/MybatisPlusDemoApplication.java
@@ -0,0 +1,18 @@
+package com.github.charlotte.stack.orm;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Daniel
+ */
+@SpringBootApplication
+@MapperScan("com.github.charlotte.stack.orm.dao")
+public class MybatisPlusDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisPlusDemoApplication.class, args);
+	}
+
+}
