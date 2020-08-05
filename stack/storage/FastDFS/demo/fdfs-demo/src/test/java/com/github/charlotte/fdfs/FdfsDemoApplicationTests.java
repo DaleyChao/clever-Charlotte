8216f6d3646d8fb11c4b64f81d63c6a5fbdd4824
Daniel <daleychao@163.com>
2020-08-05 23:48:01 +0800

    [+]oa

---
+++ stack/storage/FastDFS/demo/fdfs-demo/src/test/java/com/github/charlotte/fdfs/FdfsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.charlotte.fdfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdfsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
