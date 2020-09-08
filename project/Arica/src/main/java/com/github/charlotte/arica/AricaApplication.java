18282aee42a1d3b8e3740013d0d5493d6dd76b12
Daniel <daleychao@163.com>
2020-09-08 23:36:45 +0800

    [+]arica

--- project/Arica/src/main/java/com/github/charlotte/arica/AricaApplication.java
+++ project/Arica/src/main/java/com/github/charlotte/arica/AricaApplication.java
@@ -1,9 +1,14 @@
 package com.github.charlotte.arica;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Daniel
+ */
 @SpringBootApplication
+@MapperScan(value = "com.github.charlotte.arica.dao")
 public class AricaApplication {
 
 	public static void main(String[] args) {
