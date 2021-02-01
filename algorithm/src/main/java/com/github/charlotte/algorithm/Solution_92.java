2dbb4c2993fe985bb17306735cd6e9270e92914f
DaiChao <daichao4571@100.me>
2021-02-01 16:43:10 +0800

    [ADD]算法

---
+++ algorithm/src/main/java/com/github/charlotte/algorithm/Solution_92.java
@@ -0,0 +1,44 @@
+package com.github.charlotte.algorithm;
+
+
+//反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+//
+// 说明:
+//1 ≤ m ≤ n ≤ 链表长度。
+//
+// 示例:
+//
+// 输入: 1->2->3->4->5->NULL, m = 2, n = 4
+//输出: 1->4->3->2->5->NULL
+// Related Topics 链表
+// 👍 655 👎 0
+class Solution_92 {
+
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (m == 1) {
+            return reverseN(head, n);
+        }
+        head.next = reverseBetween(head.next, m - 1, n - 1);
+        return head;
+    }
+
+    ListNode successor = null;
+
+    public ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+            successor = head.next;
+            return head;
+        }
+        ListNode least = reverseN(head.next, n - 1);
+        head.next.next = head;
+        head.next = successor;
+        return least;
+    }
+    public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+}
